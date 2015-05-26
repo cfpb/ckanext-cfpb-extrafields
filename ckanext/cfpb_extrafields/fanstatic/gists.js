de0b48bf7d748ba83de1c30b89737ac02f9bb5ba
Sam Leitner <samuel.leitner@cfpb.gov>
2015-05-26 12:33:52 -0400

    adding js for drop down gist creation.

---
+++ ckanext/cfpb_extrafields/fanstatic/gists.js
@@ -0,0 +1,58 @@
+//<!-- http://stackoverflow.com/questions/9154026/jquery-dynamically-load-a-gist-embed -->
+document.getElementById("gistList").onchange = function(e) {
+    if (e.target.value) {
+        // Create an iframe, append it to this document where specified
+        var gistFrame = document.createElement("iframe");
+        gistFrame.setAttribute("width", "100%");
+        gistFrame.id = "gistFrame";
+        
+        var zone = document.getElementById("gistZone");
+        zone.innerHTML = "";
+        zone.appendChild(gistFrame);
+        
+        // Create the iframe's document
+        var gistFrameHTML = '<html><body onload="parent.adjustIframeSize(document.body.scrollHeight)"><scr'+'ipt type="text/javascript" src="https://gist.github.com/' + e.target.value + '.js"></sc'+'ript></b'+'ody></h'+'tml><base target="_parent" />';
+        
+
+        // Set iframe's document with a trigger for this document to adjust the height
+        var gistFrameDoc = gistFrame.document;
+        
+        
+        if (gistFrame.contentDocument) {
+            gistFrameDoc = gistFrame.contentDocument;
+        } else if (gistFrame.contentWindow) {
+            gistFrameDoc = gistFrame.contentWindow.document;
+        }
+        
+        gistFrameDoc.open();
+        gistFrameDoc.writeln(gistFrameHTML);
+        gistFrameDoc.close();
+        
+        console.log("iframe added");
+    }
+}
+
+function adjustIframeSize(newHeight) {
+    var i = document.getElementById("gistFrame");
+    i.style.height = parseInt(newHeight) + "px";
+    console.log("size adjusted", newHeight);
+}
+
+
+//
+//    <!--
+//document.getElementById("gistList").onchange = function(e) { //function loadGist(element, gistId) {
+//    var callbackName = "gist_callback";
+//    window[callbackName] = function (gistData) {
+//        delete window[callbackName];
+//        var html = '<link rel="stylesheet" href="' + $( gistData.stylesheet).text() + '"></link>';
+//        html += gistData.div;
+//        e.innerHTML = html;
+//        script.parentNode.removeChild(script);
+//    };
+//    var script = document.createElement("script");
+//    script.setAttribute("src", "https://gist.github.com/" + e.target.value + ".json?callback=" + callbackName);
+//    document.body.appendChild(script);
+//}
+//-->
+
