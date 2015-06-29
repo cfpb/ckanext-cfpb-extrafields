032abe09c652b36791944b43531d567fb2bceb2c
Sam Leitner <samuel.leitner@cfpb.gov>
2015-06-29 15:57:29 -0400

    Intermediate commit. Cant use GET for datadict so need to supply datadict in html form. first step in that direction: reverting datadict_post to simple export to hidden input field on button click.

--- ckanext/cfpb_extrafields/fanstatic/datadict_post.js
+++ ckanext/cfpb_extrafields/fanstatic/datadict_post.js
@@ -1,46 +1,66 @@
 "use strict";
 
-// A few jQuery helpers for exporting only
-var $EXPORT = $('#export');
-jQuery.fn.pop = [].pop;
-jQuery.fn.shift = [].shift;
-var $BTN = $('#export-btn');
-$BTN.click(function () {
-  var $rows = $TABLE.find('tr:not(:hidden)');
-  var header_keys = [];
-  var header_names=[]; //head
-  var record = [];
-  
-  // Get the headers (add special header logic here)
-  $($rows.shift()).find('th:not(:empty)').each(function () {
-    header_keys.push($(this).text().toLowerCase());
-    header_names.push($(this).text());
+ckan.module('datadict_post', function ($, _) {
+  // A few jQuery helpers for exporting only
+  var $EXPORT = $('#export');
+  jQuery.fn.pop = [].pop;
+  jQuery.fn.shift = [].shift;
+  return {
+    initialize: function () {
+          $.proxyAll(this, /_on/);
+          this.el.on('click', this._onClick);
+    },
+    _onClick: function(event) {
+/*            this.options.gistdesc = $('#gist-description').val();
+      if (this.options.gistdesc == ''){
+          var outhtml = '<h3><font color="red">Fill in description field.</font></h3>';
+          $('#ace_output').html(outhtml);
+          $('#gist-description').focus();
+          return;
+      }
+      */
+  ///////////////////////
+      var $rows = $TABLE.find('tr:not(:hidden)');
+      var header_keys = [];
+      var header_names=[]; //head
+      var record = [];
+      
+      // Get the headers (add special header logic here)
+      $($rows.shift()).find('th:not(:empty)').each(function () {
+        header_keys.push($(this).text().toLowerCase());
+        header_names.push($(this).text());
+      });
 
-  });
+      //header_names are the first element (and remain in an ordered array)
+      record.push(header_names);
+      record.push(header_keys);
+      // Turn all existing rows into a loopable array
+      $rows.each(function () {
+        var $td = $(this).find('td');
+        var h = {};
+        
+        // Use the header_keys from earlier to name our hash keys
+        header_keys.forEach(function (header_key, i) {
+          h[header_key] = $td.eq(i).text();   
+        });
+        
+        record.push(h);
+      });
+      
+      // Output the result
+      record = JSON.stringify(record);
+      $EXPORT.text(record);
+      $( "input:hidden[name=datadict]" ).val(record);
+    },
+  }
+});
 
-  //header_names are the first element (and remain in an ordered array)
-  record.push(header_names);
-  record.push(header_keys);
-  // Turn all existing rows into a loopable array
-  $rows.each(function () {
-    var $td = $(this).find('td');
-    var h = {};
-    
-    // Use the header_keys from earlier to name our hash keys
-    header_keys.forEach(function (header_key, i) {
-      h[header_key] = $td.eq(i).text();   
-    });
-    
-    record.push(h);
-  });
-  
-  // Output the result
-  $EXPORT.text(JSON.stringify(record));
+    ///////////////////////
 
-  this.sandbox.client.getTemplate('datadict_post.html',
-                                  record,
-                                  null);
+
+// var $BTN = $('#export-btn');
+// $BTN.click(function () {
                                  
 
-});
+
 
