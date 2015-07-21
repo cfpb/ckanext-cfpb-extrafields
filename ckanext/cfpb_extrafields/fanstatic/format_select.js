365f9d8def701ec8f8daba7647bdface7415eb8d
Sam Leitner <samuel.leitner@cfpb.gov>
2015-07-21 10:42:57 -0400

    comment on warning box

--- ckanext/cfpb_extrafields/fanstatic/format_select.js
+++ ckanext/cfpb_extrafields/fanstatic/format_select.js
@@ -1,5 +1,8 @@
+// this generates a warning about unsaved data from CKAN
+// public/base/javascript/modules/basic-form.js
 $(function() {
     $('#field-format').change( function() {
         $( "#resource_save" ).trigger( "click" );
     });
 });
+
