c5f3c70ce7203d89f7b411fc9e1d2a2f8a89dbf8
Matthew Boehm <boehm.matthew@gmail.com>
2016-12-16 16:40:18 -0500

    Dig import: create ckan package for the dataset
    Upon successful upload/extraction, call package_create to actually add
    the dataset to the system

--- ckanext/cfpb_extrafields/controllers/digimport.py
+++ ckanext/cfpb_extrafields/controllers/digimport.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     from stringIO import StringIO
 import json
+import re
 import urllib
 
 from ckan.plugins.toolkit import BaseController, redirect_to, render, request, response
@@ -10,16 +11,16 @@ import ckanapi
 
 from ckanext.cfpb_extrafields.digutils import make_rec
 
-# def get_datasets(rows=10000):
-    # api = ckanapi.LocalCKAN()
-    # result = api.call_action(
-        # "package_search",
-        # {
-            # "q": "",
-            # "rows": rows,
-        # }
-    # )
-    # return result
+def make_name(title):
+    return re.sub(r"[^a-zA-Z0-9]*", "-", title).lower()
+
+def upload_rec(rec):
+    api = ckanapi.LocalCKAN()
+    result = api.call_action(
+        "package_create",
+        rec
+    )
+    return result
 
 class ImportController(BaseController):
     def index(self, group):
@@ -37,4 +38,10 @@ class ImportController(BaseController):
         rec, errors = make_rec(dig)
         if errors:
             redirect_to("import_page", errors=json.dumps(errors), group=group)
+        else:
+            rec["owner_org"] = group
+            rec["name"] = make_name(rec["title"])
+            rec["notes"] = "Automatically import from DIG"
+            upload_rec(rec)
+            import logging;logging.error(repr(rec))
         return json.dumps(rec)
