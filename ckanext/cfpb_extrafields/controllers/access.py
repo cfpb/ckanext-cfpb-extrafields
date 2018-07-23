17c320e2d94bd0919cf532a91fdcf0cb03eb7052
Tim Anderegg <timothy.anderegg@cfpb.gov>
2018-07-23 14:51:03 -0400

    WIP request access controller.

--- ckanext/cfpb_extrafields/controllers/access.py
+++ ckanext/cfpb_extrafields/controllers/access.py
@@ -1,4 +1,5 @@
-from ckan.plugins.toolkit import BaseController, get_action, render, c as context
+from ckan.plugins.toolkit import BaseController, get_action, render, c as context, flash_error
+import requests
 
 class AccessController(BaseController):
     def index(self, resource_id, cn):
@@ -13,4 +14,22 @@ class AccessController(BaseController):
         return render('ckanext/cfpb-extrafields/access_index.html', {"resource": resource, "package": package, "cn": cn, "context": context})
 
     def submit(self, resource_id, cn):
-        return
+        try:
+            workflow_url = config['ckanext.access.workflow_url']
+        except KeyError:
+            flash_error("Please set ckanext.access.workflow_url in order to submit access requests.")
+            redirect_to("request_access", resource_id=resource_id, package_id=package_id)
+
+        resource = get_action('resource_show')({}, data_dict={
+            'id': resource_id
+        })
+
+        package = get_action('package_show')({}, data_dict={
+            'id': resource['package_id']
+        })
+
+        redirect_to("resource_read", id=resource.id)
+
+        #response = requests.post(
+#
+#        )
