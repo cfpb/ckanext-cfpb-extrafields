8a36ed0b204220fadcad65ed30dbdb4076a2cb07
Tim Anderegg <timothy.anderegg@cfpb.gov>
2018-09-24 14:04:49 -0400

    Adds missing comma.

--- ckanext/cfpb_extrafields/exportutils.py
+++ ckanext/cfpb_extrafields/exportutils.py
@@ -56,7 +56,7 @@ FIELDS = [
     ("privacy_sorn_number", "Privacy: SORN number"),
     ("records_retention_schedule", "Records retention schedule"),
     ("procurement_document_id", "Procurement document ID"),
-    ("obligation", "Obligation")
+    ("obligation", "Obligation"),
     ("transfer_date", "Transfer: Date"),
     ("data_governance_notes", "Data Governance: Notes"),
     ("legal_notes","Legal: Notes"),
